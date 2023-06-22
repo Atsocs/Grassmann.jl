9e3d784e8a1692d83b2b3da4acc0f50351baa2c6
Caio <atsocs.cc@gmail.com>
2023-06-22 19:42:41 -0300

    update example

--- examples/basic.jl
+++ examples/basic.jl
@@ -1,11 +1,8 @@
-include("Grassmann.jl")
-using .Grassmann
+include("../Grassmann.jl")
 
-e1, e2, e3, e4 = basis(4)
-
+e1, e2, e3, e4 = Grassmann.basis(4)
 v = e1 * e2 + e2 * e3 + e3 * e4 - e1 * e4
 
 # verify that v * v == 0
-
 println("v = ", v)
 println("v * v = ", v * v)
